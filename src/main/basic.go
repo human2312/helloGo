2c30a418fafc354cb3f59e086cd09eb6f9b2f0af
L.M.Y <human2312@qq.com>
2019-11-22 17:28:44 +0800

    reformat code

--- src/main/basic.go
+++ src/main/basic.go
@@ -10,30 +10,30 @@ import (
 
 const constant string = "constant"
 
-func main()  {
-	s,i,f,b := str()
+func main() {
+	s, i, f, b := str()
 	rand.Seed(time.Now().Unix())
 	randnum := rand.Intn(4)
-	fmt.Println(s,i,f,b)
+	fmt.Println(s, i, f, b)
 	fmt.Println(constant)
 	fmt.Println(example(randnum))
 	fmt.Println("done...")
 }
 
 //str some basic string
-func str() (s string,i int,f float64,b bool) {
+func str() (s string, i int, f float64, b bool) {
 	s = "Lemy" + " learn golang."
-	i = 1+2
+	i = 1 + 2
 	f = 7.0 / 3.1
 	b = !true
 	return
 }
 
-func example(i int) (result string)   {
+func example(i int) (result string) {
 	if i >= 0 && i <= 2 {
 		switch i {
 		case 1:
-			result = "you chose "+ strconv.Itoa(i)
+			result = "you chose " + strconv.Itoa(i)
 		case 2:
 			result = "you chose mid num " + strconv.Itoa(i)
 		default:
