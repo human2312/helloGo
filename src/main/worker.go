df43ddcab817d34832a0950a0cd0a1de32e89d06
L.M.Y <human2312@qq.com>
2019-11-26 14:19:13 +0800

    refomart

--- src/main/worker.go
+++ src/main/worker.go
@@ -6,34 +6,34 @@ import (
 )
 
 //工作池
-func worker(id int,jobs <- chan int ,result chan<- int)  {
+func worker(id int, jobs <-chan int, result chan<- int) {
 	for j := range jobs {
-		fmt.Println("worker",id,"processing job",j)
+		fmt.Println("worker", id, "processing job", j)
 		time.Sleep(time.Second)
 		result <- j * 2
 	}
 }
 
-func main()  {
-	jobs := make(chan int,100)
-	results := make(chan int,100)
+func main() {
+	jobs := make(chan int, 100)
+	results := make(chan int, 100)
 
-	num := 1000 //任务数
+	num := 1000     //任务数
 	worknum := 4000 //进程数
 
 	//启动三个worker工作池
-	for w:=1;w <= worknum;w++ {
-		go worker(w,jobs,results)
+	for w := 1; w <= worknum; w++ {
+		go worker(w, jobs, results)
 	}
 
 	//发送9个jobs任务
-	for j := 1;j <=num ;j++{
+	for j := 1; j <= num; j++ {
 		jobs <- j
 	}
 	close(jobs)
 
 	//输出jobs任务结果
-	for a := 1;a <= num; a++ {
+	for a := 1; a <= num; a++ {
 		<-results
 	}
 }
