096dd5b1e21fec3bc5362d9a1aaed10f412c145c
L.M.Y <human2312@qq.com>
2019-11-26 14:04:58 +0800

    工作池

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # helloGo
 个人go 实训题
+
+注：实例无法go build ,存多个main ..所以go run的时候只能go某个项目跑
--- src/main/worker.go
+++ src/main/worker.go
@@ -18,19 +18,22 @@ func main()  {
 	jobs := make(chan int,100)
 	results := make(chan int,100)
 
+	num := 1000 //任务数
+	worknum := 4000 //进程数
+
 	//启动三个worker工作池
-	for w:=1;w <= 3;w++ {
-		go worker(1,jobs,results)
+	for w:=1;w <= worknum;w++ {
+		go worker(w,jobs,results)
 	}
 
 	//发送9个jobs任务
-	for j := 1;j <=9 ;j++{
+	for j := 1;j <=num ;j++{
 		jobs <- j
 	}
 	close(jobs)
 
 	//输出jobs任务结果
-	for a := 1;a <= 9; a++ {
+	for a := 1;a <= num; a++ {
 		<-results
 	}
 }
